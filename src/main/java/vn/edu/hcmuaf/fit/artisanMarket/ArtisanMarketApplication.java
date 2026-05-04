82051977cb29cebd513608fc59267cd992042ddd
hoangquocluat88 <hoangquocluat88@gmail.com>
2026-05-04 11:19:57 +0700

    first commit

---
+++ src/main/java/vn/edu/hcmuaf/fit/artisanMarket/ArtisanMarketApplication.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.artisanMarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisanMarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisanMarketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/edu/hcmuaf/fit/artisanMarket/ArtisanMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.edu.hcmuaf.fit.artisanMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtisanMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
